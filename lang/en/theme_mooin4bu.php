95628b41eccecfe16d76eaa7772cdc0def2e8c7b
tinjohn <tina.john@th-luebeck.de>
2024-10-30 20:09:39 +0100

    initial commit

---
+++ lang/en/theme_mooin4bu.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Boost Union Child - Language pack
+ *
+ * @package    theme_mooin4bu
+ * @copyright  2023 Daniel Poggenpohl <daniel.poggenpohl@fernuni-hagen.de> and Alexander Bias <bias@alexanderbias.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Let codechecker ignore some sniffs for this file as it is perfectly well ordered, just not alphabetically.
+// phpcs:disable moodle.Files.LangFilesOrdering.UnexpectedComment
+// phpcs:disable moodle.Files.LangFilesOrdering.IncorrectOrder
+
+// General.
+$string['pluginname'] = 'Mooin4 Boost Union Child';
+$string['choosereadme'] = 'This plugin is just a boilerplate template one can use to develop Boost Union child themes.';
+$string['configtitle'] = 'Mooin4 Boost Union Child';
+$string['settingsoverview_buc_desc'] = 'Mooin4 customizes Boost Union.';
+
+// Settings: General settings tab.
+// ... Section: Inheritance.
+$string['inheritanceheading'] = 'Inheritance';
+$string['inheritanceinherit'] = 'Inherit';
+$string['inheritanceduplicate'] = 'Duplicate';
+$string['inheritanceoptionsexplanation'] = 'Most of the time, inheriting will be perfectly fine. However, it may happen that imperfect code is integrated into Boost Union which prevents simple SCSS inheritance for particular Boost Union features. If you encounter any issues with Boost Union features which seem not to work in Boost Union Child as well, try to switch this setting to \'Dupliate\' and, if this solves the problem, report an issue on Github (see the README.md file for details how to report an issue).';
+// ... ... Setting: Pre SCSS inheritance setting.
+$string['prescssinheritancesetting'] = 'Pre SCSS inheritance';
+$string['prescssinheritancesetting_desc'] = 'With this setting, you control if the pre SCSS code from Boost Union should be inherited or duplicated.';
+// ... ... Setting: Extra SCSS inheritance setting.
+$string['extrascssinheritancesetting'] = 'Extra SCSS inheritance';
+$string['extrascssinheritancesetting_desc'] = 'With this setting, you control if the extra SCSS code from Boost Union should be inherited or duplicated.';
+
+/**************************************************************
+ * EXTENSION POINT:
+ * Add your language strings for your settings here.
+ *************************************************************/
+
+// Privacy API.
+$string['privacy:metadata'] = 'Mooin4 Boost Union Child theme does not store any personal data about any user.';
---
+++ version.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Boost Union Child - Version file
+ *
+ * @package    theme_mooin4bu
+ * @copyright  2023 Daniel Poggenpohl <daniel.poggenpohl@fernuni-hagen.de> and Alexander Bias <bias@alexanderbias.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_mooin4bu';
+$plugin->version = 2024103001;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = ['theme_boost_union' => 2023010548];
